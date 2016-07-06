5e964c11b5161b13d49095883b6e1ac95ee5de6e
Arkainoh <arkainoh@gmail.com>
2016-07-06 06:36:42 +0000

    login service and post are added

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def updateDB
+    # -- logic --
+    
+    # Crawl !
+  
+    # Store data into model
+    
+    # redirect to the prev page
+  
+  end
+
+  
+end
