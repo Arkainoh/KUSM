e7111b98c6977e025c813787f815d7f38b24aa38
Arkainoh <arkainoh@gmail.com>
2016-07-07 05:39:50 +0000

    Parsing JSON completed

--- crawl_test.rb
+++ crawl_test.rb
@@ -1,1 +1,13 @@
 # For test
+# require 'nokogiri'
+require 'open-uri'
+require 'json'
+
+# "https://yeyak.korea.ac.kr/cop/facilityUniv/facilityUnivUserResView.do?siteId=reservation&facilitySeq=21641&year=2016&month=7&day=9&hourSeq=28463"
+#doc = Nokogiri::HTML(open("https://yeyak.korea.ac.kr/cop/facilityUniv/facilityUnivUserResView.do?siteId=reservation&facilitySeq=21641&year=2016&month=7&day=9&hourSeq=28463"))
+
+data = JSON.load(open("https://yeyak.korea.ac.kr/cop/facilityUniv/facilityUnivUserResView.do?siteId=reservation&facilitySeq=21641&year=2016&month=7&day=9&hourSeq=28463"))
+
+data.each do |x|
+    puts x["groupName"] if x["status"] == "OK"
+end
